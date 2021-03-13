21e7e922b93fe4085e7ace47a5bbee530fde21fd
wiaf <killing_joke@web.de>
2021-03-13 16:26:13 +0100

    merge

---
+++ ansible/roles/common/files/youtube-dl.sh
@@ -0,0 +1,69 @@
+
+# This file is
+# {{ ansible_managed }}
+# /bin/bash
+#
+#run as root
+
+
+mkdir -p /home/wiaf/pron
+mkdir -p /home/wiaf/pron/channels
+cd /home/wiaf/pron/channels/
+youtube-dl -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' --download-archive archive.txt https://xhamster.com/users/spermboy6969/videos
+youtube-dl -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' --download-archive archive.txt https://xhamster.com/users/natashtaed/videos
+youtube-dl -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' --download-archive archive.txt https://de.xhamster.com/users/natashtaed/videos
+youtube-dl -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' --download-archive archive.txt https://de.xhamster.com/users/karlakumkueen/videos
+youtube-dl -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' --download-archive archive.txt https://de.xhamster.com/users/gregoriannatv/videos
+youtube-dl -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' --download-archive archive.txt https://de.xhamster.com/users/sissyhypnoslut/videos
+youtube-dl -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' --download-archive archive.txt https://de.xhamster.com/users/hail_satan1/videos
+youtube-dl -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' --download-archive archive.txt https://de.xhamster.com/users/redmachine69
+youtube-dl -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' --download-archive archive.txt https://de.xhamster.com/users/metal_fap/videos
+youtube-dl -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' --download-archive archive.txt https://de.xhamster.com/users/ayrtonp/videos
+youtube-dl -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' --download-archive archive.txt https://xhamster.com/users/natashtaed/videos
+youtube-dl -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' --download-archive archive.txt https://de.xhamster.com/users/pmvcollection/videos
+youtube-dl -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' --download-archive archive.txt https://xhamster.com/users/bbcdawg512/videos
+youtube-dl -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' --download-archive archive.txt https://de.xhamster.com/users/pmvcollection
+youtube-dl -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' --download-archive archive.txt https://xhamster.com/users/sugar_cravsitt/videos
+youtube-dl -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' --download-archive archive.txt https://xhamster.com/users/jyreporn/videos
+youtube-dl -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' --download-archive archive.txt https://de.pornhub.com/model/sladkislivki/videos
+
+#when $1 != '0'
+cd /home/wiaf/pron/ && mkdir -p sissytrainer && cd sissytrainer/
+mkdir -p sucktrainer && cd sucktrainer/
+youtube-dl https://de.xhamster.com/videos/pmv-for-sissy-only-13338460
+youtube-dl https://xhamster.com/videos/special-cum-in-throats-hentai-edition-9305757
+youtube-dl https://xhamster.com/videos/cum-in-her-throat-1-6596376
+youtube-dl https://xhamster.com/videos/cum-in-her-throat-2-6920199
+youtube-dl https://de.xhamster.com/videos/cum-in-mouth-compilation-4306676
+youtube-dl https://de.xhamster.com/videos/oral-cream-pies-one-5317835
+youtube-dl https://xhamster.com/videos/sloppy-blowjob-meta-compilation-sissy-cock-sucking-brainwa-13700082
+youtube-dl https://de.xhamster.com/videos/sucking-for-cum-3255109
+youtube-dl https://xhamster.com/videos/pmv-for-sissy-only-13338460
+youtube-dl https://de.xhamster.com/videos/sissy-cum-slut-bukkake-hypno-13717671
+youtube-dl https://de.xhamster.com/videos/lusting-for-huge-cocks-hd-compilation-xhDYKHJ
+youtube-dl https://de.xhamster.com/videos/sissy-suck-slut-sissy-hypno-slut-xhunG4d
+youtube-dl https://de.xhamster.com/videos/ultimate-cock-worship-4x4-compilation-11609082
+youtube-dl https://de.xhamster.com/videos/sissy-cum-slut-bukkake-hypno-13717671
+youtube-dl https://de.xhamster.com/videos/the-best-cock-worship-edit-xhcOWln
+youtube-dl https://xhamster.com/videos/cum-deep-in-the-throat-amazing-compilation-7759403
+youtube-dl https://xhamster.com/videos/mouth-creampies-the-ultimate-compilation-2-3344391
+
+cd /home/wiaf/pron/ && kdir -p pmv && cd pmv
+
+cd ..
+
+cd /home/wiaf/pron/sort/
+youtube-dl https://xhamster.com/videos/german-amateur-bitch-3some-in-roulette-no-condom-userdate-10912510
+
+
+
+mkdir -p /home/wiaf/music
+cd /home/wiaf/music/
+youtube-dl -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' https://soundcloud.com/ellen-allien/tracks
+mkdir -p test1/
+cd test1/
+youtube-dl -x -o '%(uploader) - %(title)s.%(ext)s' https://www.youtube.com/user/EllenAllienTV/videos
+cd /home/wiaf/music/
+mkdir -p /test2/
+cd test2/
+youtube-dl -x --audio-format wav --audio-quality 0 --archive archive.txt -o '%(uploader) - %(title)s.%(ext)s' https://www.youtube.com/user/EllenAllienTV/videos
