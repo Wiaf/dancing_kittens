7e786d60414bbe85510cc7e2c73dc7d004f77657
Wiaf <killing_joke@web.de>
2021-09-03 11:26:31 +0200

    rework samba

---
+++ ansible/roles/samba/files/btrfs_maintenance.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#
+# paranoia settings
+#
+umask 022
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+export PATH
+
+sudo btrfs scrub start -B /var/kittycastle/ | sudo tee /var/log/btrfs-scrub.log
+
+(( m = 0 ))
+
+while sudo btrfs balance start -musage=$m /var/kittycastle
+do
+        (( m = m + 5 ))
+done
+
+(( d = 0 ))
+
+while sudo btrfs balance start -dusage=$d /var/kittycastle
+do
+        (( d = d + 5 ))
+done
+
+btrfs balance start /var/kittycastle
+^
+btrfs scrub start -Bd /var/kittycastle
+
+exit 0
